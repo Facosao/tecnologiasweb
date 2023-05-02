6a4c016c0b74a060f739b1b32682ea83f16132c4
Facosao <leonardosalgado02@hotmail.com>
2023-05-02 09:36:01 -0300

    Upload dos arquivos

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+/* 
+O código verifica se o token de autenticação do usuário está armazenado no localStorage. 
+Se não estiver, uma mensagem de alerta é exibida e o usuário é redirecionado para a página de login. 
+*/ 
+if (localStorage.getItem("token") == null) { 
+    alert("Você precisa estar logado para acessar essa página"); 
+    window.location.href = "./assets/html/signin.html";
+  }
+  
+  const userLogado = JSON.parse(localStorage.getItem("userLogado"));
+  
+  const logado = document.querySelector("#logado");
+  logado.innerHTML = `Olá ${userLogado.nome}`;
+  
+  function sair() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userLogado");
+    window.location.href = "./assets/html/signin.html";
+  }
