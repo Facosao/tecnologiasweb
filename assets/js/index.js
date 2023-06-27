cc5ff1cb858e87bf82c4d1392645475275fae71a
Facosao <leonardosalgado02@hotmail.com>
2023-06-27 08:48:23 -0300

    Adição do botao "Sair" em todas as páginas

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,17 @@
 /* 
 O código verifica se o token de autenticação do usuário está armazenado no localStorage. 
 Se não estiver, uma mensagem de alerta é exibida e o usuário é redirecionado para a página de login. 
-*/ 
+*/
+
+var signin_str = "./assets/html/signin.html"; 
+
+if (!window.location.href.endsWith("index.html")) {
+  signin_str = "./signin.html";
+}
+
 if (localStorage.getItem("token") == null) { 
-    alert("Você precisa estar logado para acessar essa página"); 
-    window.location.href = "./assets/html/signin.html";
+    alert("Você precisa estar logado para acessar essa página");
+    window.location.href = signin_str;
 } else {
   // TODO: Adequar estilo para o nome em HTML
   const userLogado = JSON.parse(localStorage.getItem("userLogado"));
@@ -15,5 +22,5 @@ if (localStorage.getItem("token") == null) {
 function Sair() {
   localStorage.removeItem("token");
   localStorage.removeItem("userLogado");
-  window.location.href = "./assets/html/signin.html";
+  window.location.href = signin_str;
 }
